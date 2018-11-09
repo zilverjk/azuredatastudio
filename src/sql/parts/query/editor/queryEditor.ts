b80cbf9204d1265f2e0a117882cd8eb1e8fcab23
Anthony Dresser <anthonydresserdev@gmail.com>
2018-11-08 21:24:24 -0800

    got it working

--- src/sql/parts/query/editor/queryEditor.ts
+++ src/sql/parts/query/editor/queryEditor.ts
@@ -134,9 +134,6 @@ export class QueryEditor extends BaseEditor {
 			return TPromise.as(undefined);
 		}
 
-		// Make sure all event callbacks will be sent to this QueryEditor in the case that this QueryInput was moved from
-		// another QueryEditor
-
 		return TPromise.join([
 			super.setInput(newInput, options, token),
 			this.taskbar.setInput(newInput),
@@ -145,8 +142,10 @@ export class QueryEditor extends BaseEditor {
 		]).then(() => {
 			dispose(this.inputDisposables);
 			this.inputDisposables = [];
-			this.inputDisposables.push(this.input.onQueryStart(() => {
-				this.addResultsEditor();
+			this.inputDisposables.push(this.input.state.onChange(c => {
+				if (c.executingChange && this.input.state.executing) {
+					this.addResultsEditor();
+				}
 			}));
 		});
 	}
