b1e82ceec435fc90bf419f43462ed668aad81a63
Raj <44002319+rajmusuku@users.noreply.github.com>
2018-11-28 10:32:40 -0800

    Revert "3240: Empty cells show thick border (#3242)"
    This reverts commit 3952fdbe2d1444c20d43b456f2180828a9814cce.

--- src/sql/parts/notebook/cellViews/outputArea.component.ts
+++ src/sql/parts/notebook/cellViews/outputArea.component.ts
@@ -3,12 +3,9 @@
 *  Licensed under the Source EULA. See License.txt in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 import 'vs/css!./code';
-import 'vs/css!./outputArea';
-import { OnInit, Component, Input, Inject, ElementRef, ViewChild, forwardRef, ChangeDetectorRef } from '@angular/core';
+import { OnInit, Component, Input, Inject, forwardRef, ChangeDetectorRef } from '@angular/core';
 import { AngularDisposable } from 'sql/base/common/lifecycle';
 import { ICellModel } from 'sql/parts/notebook/models/modelInterfaces';
-import * as themeColors from 'vs/workbench/common/theme';
-import { IWorkbenchThemeService, IColorTheme } from 'vs/workbench/services/themes/common/workbenchThemeService';
 
 export const OUTPUT_AREA_SELECTOR: string = 'output-area-component';
 
@@ -17,30 +14,20 @@ export const OUTPUT_AREA_SELECTOR: string = 'output-area-component';
 	templateUrl: decodeURI(require.toUrl('./outputArea.component.html'))
 })
 export class OutputAreaComponent extends AngularDisposable implements OnInit {
-	@ViewChild('outputarea', { read: ElementRef }) private outputArea: ElementRef;
 	@Input() cellModel: ICellModel;
 
 	private readonly _minimumHeight = 30;
 
 	constructor(
-		@Inject(IWorkbenchThemeService) private themeService: IWorkbenchThemeService,
-		@Inject(forwardRef(() => ChangeDetectorRef)) private _changeRef: ChangeDetectorRef
+		@Inject(forwardRef(() => ChangeDetectorRef)) private _changeRef: ChangeDetectorRef,
 	) {
 		super();
 	}
-
-	ngOnInit() {
-		this._register(this.themeService.onDidColorThemeChange(this.updateTheme, this));
-		this.updateTheme(this.themeService.getColorTheme());
+	ngOnInit(): void {
 		if (this.cellModel) {
 			this.cellModel.onOutputsChanged(() => {
 				this._changeRef.detectChanges();
 			});
 		}
-	}
-
-	private updateTheme(theme: IColorTheme): void {
-		let outputElement = <HTMLElement>this.outputArea.nativeElement;
-		outputElement.style.borderTopColor = theme.getColor(themeColors.SIDE_BAR_BACKGROUND, true).toString();
 	}
 }
